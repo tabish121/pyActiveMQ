a4b4f464bf9484bd653df7af1e1e7b699d4c0400
fullung <fullung@fac7dada-cd26-0410-acf4-7b34770fff89>
2007-06-13 01:16:18 +0000

    Put incomplete tests in a separate file.

--- src/test/test.py
+++ src/test/test.py
@@ -235,40 +235,6 @@ class _test_any_protocol:
         del producer
         self.assertEqual(2, sys.getrefcount(session))
 
-    def xtest_durable_consumer(self):
-        conn1 = self.conn
-        del self.conn
-        session1 = conn1.createSession()
-        sub = "mysubscription1"
-        # unsubscribing non-existant subscription should fail
-        self.assertRaises(UserWarning, session1.unsubscribe, sub)
-        topic = self.random_topic(session1)
-        consumer = session1.createDurableConsumer(topic, sub, '', False)
-        # unsubscribing with an active consumer should fail
-        self.assertRaises(UserWarning, session1.unsubscribe, sub)
-        del consumer
-        # TODO this shouldn't throw
-        #session1.unsubscribe(sub)
-        consumer = session1.createDurableConsumer(topic, sub, '', False)
-
-        from pyactivemq import ActiveMQConnectionFactory
-        f = ActiveMQConnectionFactory(self.url)
-        conn2 = f.createConnection()
-        session2 = conn2.createSession()
-        # Using a topic created in another session to create the
-        # producer. This shouldn't cause problems.
-        producer = session2.createProducer(topic)
-
-        conn1.start()
-        conn2.start()
-
-        # TODO send a few messages using the producer
-        # TODO receive messages in a transaction
-        # TODO rollback transaction
-        # TODO close connection
-        # TODO reconnect conn1 and setup subscription
-        # check that messages are still there
-
     def test_TextMessage(self):
         session = self.conn.createSession()
         textMessage = session.createTextMessage()
@@ -371,15 +337,6 @@ class _test_any_protocol:
 
         msg = consumer.receive(50)
         self.assert_(msg is None)
-
-    def xtest_MessageProducer_sends(self):
-        # test all send methods of MessageProducer
-        # make consumers on 2 topics so that we can check that the
-        # topic specified when calling send instead of topic specified
-        # when creating producer gets used
-
-        # test that sending to a closed connection fails or something
-        pass
 
     def test_BytesMessage_bodyBytes(self):
         session = self.conn.createSession()
